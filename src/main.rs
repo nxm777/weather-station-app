173568cbaa81b27afc91d44870b8d989a7ca09ad
nxm777 <KamilTatara4@gmail.com>
2026-04-19 00:43:08 +0200

    feat(server): setup basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+    let host = env::var("SERVER_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port = env::var("SERVER_PORT").unwrap_or_else(|_| "3000".to_string());
+    let address = format!("{}:{}", host, port);
+
+    let app = Router::new().route("/", get(health_check));
+    let listener = TcpListener::bind(&address)
+        .await
+        .expect("Failed to start a server");
+
+    println!("Server is running at http://{}", address);
+
+    
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health_check() -> &'static str {
+    "Server is running"
 }
